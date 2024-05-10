cac43411c222071f0c949234e25394cd46a2d5d0
Nicolás García <nicolasg.ngl@gmail.com>
2024-05-10 13:06:42 -0500

    fetching data in mainProducts component

--- src/components/Home/MainProducts/MainProducts.tsx
+++ src/components/Home/MainProducts/MainProducts.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import {SHOPIFY_API_KEY, SHOPIFY_HOSTNAME } from '../../../env'
 
-export const MainProducts = () => {
+
+const getProducts = async () => {
+  const res = await fetch (
+    `https://${SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+      headers:new Headers({
+        'X-Shopify-Access-Token': SHOPIFY_API_KEY || ""
+      })
+    })
+    const data = await res.json();
+    return data
+}
+
+export const MainProducts = async () => {
+  const products = await getProducts();
+  console.log(products);
+  
   return (
     <div>MainProducts</div>
   )
