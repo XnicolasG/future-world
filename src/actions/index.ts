6db9fe3a6f0ef1149d749b5242fe4ac3a8926bf8
Nicolás García <nicolasg.ngl@gmail.com>
2024-06-12 16:30:49 -0500

    process to sing up in shopify via graphql added.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,20 @@
 'use server'
-export const handleCreateUser = (formData:any) =>{
-    console.log(formData);
+
+import { GraphQLClientSingleton } from "app/graphql";
+import { createUserMutation } from "app/graphql/mutations/createUserMutation";
+
+export const handleCreateUser = async (formData: FormData) => {
+    const formDataObject = Object.fromEntries(formData)
+    delete formDataObject["password_confirmation"]
+    const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+    const variables = {
+        input : {
+            ...formDataObject,
+            phone: '+57' + formDataObject.phone
+        }
+    }
+    //error en linea de const data, invalid url
+    const data = await graphqlClient.request(createUserMutation, variables)
+    console.log(data);
+    
 }
