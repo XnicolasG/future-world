09f74f07dfee7c7c627cce88b605441b83a280a9
Nicolás García <nicolasg.ngl@gmail.com>
2024-08-01 17:53:33 -0500

    userinfo component updated

--- src/app/myAccount/@userInfo/page.tsx
+++ src/app/myAccount/@userInfo/page.tsx
@@ -1,7 +1,7 @@
 import { validateAccessToken } from 'app/utilities/auth/validateAccessToken'
 import React from 'react'
 
-
+export const dynamic = 'force-dynamic';
 
 export default async function MyAccountPage() {
   const customer = await validateAccessToken();
